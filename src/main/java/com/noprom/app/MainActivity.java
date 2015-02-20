e780ea531deb6ae4659dd09b5fa2ca57b2f61738
noprom <874702417@qq.com>
2015-02-20 17:23:00 +0800

    initView done
    initView done

--- src/main/java/com/noprom/app/MainActivity.java
+++ src/main/java/com/noprom/app/MainActivity.java
@@ -1,39 +1,95 @@
 package com.noprom.app;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private ViewPager mViewPager;
+    private PagerAdapter mAdapter;
+    private List<View> mViews = new ArrayList<View>();
+
+    private LinearLayout mTabWeixin;
+    private LinearLayout mTabFriend;
+    private LinearLayout mTabAddress;
+    private LinearLayout mTabSetting;
+
+    private ImageButton mWeixinImg;
+    private ImageButton mFriendImg;
+    private ImageButton mAddressImg;
+    private ImageButton mSettingImg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+        // Tabs
+        mTabWeixin = (LinearLayout) findViewById(R.id.id_tab_weixin);
+        mTabFriend = (LinearLayout) findViewById(R.id.id_tab_frd);
+        mTabAddress = (LinearLayout) findViewById(R.id.id_tab_address);
+        mTabSetting = (LinearLayout) findViewById(R.id.id_tab_settings);
+
+        // ImageButton
+        mWeixinImg = (ImageButton) findViewById(R.id.id_tab_weixin_img);
+        mFriendImg = (ImageButton) findViewById(R.id.id_tab_frd_img);
+        mAddressImg = (ImageButton) findViewById(R.id.id_tab_address_img);
+        mSettingImg = (ImageButton) findViewById(R.id.id_tab_settings_img);
+
+        LayoutInflater mInflater = LayoutInflater.from(this);
+        View tab01 = mInflater.inflate(R.layout.tab01, null);
+        View tab02 = mInflater.inflate(R.layout.tab02, null);
+        View tab03 = mInflater.inflate(R.layout.tab03, null);
+        View tab04 = mInflater.inflate(R.layout.tab04, null);
+        mViews.add(tab01);
+        mViews.add(tab02);
+        mViews.add(tab03);
+        mViews.add(tab04);
+
+        mAdapter = new PagerAdapter() {
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView(mViews.get(position));
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                View view = mViews.get(position);
+                container.addView(view);
+                return view;
+            }
+
+            @Override
+            public int getCount() {
+                return mViews.size();
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+        };
+
+        mViewPager.setAdapter(mAdapter);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
